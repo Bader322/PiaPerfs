1004517c74d8dc674850ac7da337718e3e069fa4
bader <bader322>
2020-03-23 12:42:30 -0700

    adding another stage, still have to go back to a working version

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const edge = require('edge.js')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+edge.registerViews(path.join(__dirname, './views'))
 
 var app = express();
 
-app.use(logger('dev'));
+
+app.set('view engine', edge)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
