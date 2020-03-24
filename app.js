7971a276f417e1a7436d9dcc907d26596e974231
bader <bader322>
2020-03-23 18:45:09 -0700

    switched app to use Edge templating engine

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./controllers/index');
+const usersRouter = require('./controllers/users');
+const postsRouter = require('./controllers/posts');
+const app = express();
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+const { config, engine } = require('express-edge');
+app.use(engine);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts',postsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
