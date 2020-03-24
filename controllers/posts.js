268afb59b78e714ca26486036fd82ddb66ad73e7
bader <bader322>
2020-03-23 23:25:36 -0700

    wanting to print a loop of posts in posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,38 +1,27 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/pianoperf';
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/pianoref';
 const router = express.Router();
-const Post = require('../Models/Post');
+const post = require('../Models/Post')
 // Database Name
 const dbName = 'posts';
 
 
 // route to posts controller
-router.get('/' , (req, res, next) => {
-  const posts = Post.find({});
-  // console.log("POsts::  "+posts)
-  res.render('posts',posts);
-  
-  mongoose.connect(url, { useNewUrlParser: true })
-    .then(() => console.log('You are now connected to Mongo!'))
-    .catch(err => console.error('Something went wrong', err))   
-   
-  
-  
-  
-  
-  // Use connect method to connect to the server
-      // MongoClient.connect(url, function(err, client) {
-      //   console.log("Connected successfully to server");
-      //   const db = client.db(dbName);
-      //   const collection = db.collection('videos');
-      //   collection.find().toArray((error, documents) => {
-      //   console.log(documents);
-      //   });
+router.get('/' ,(req, res, next) => {
+  res.render('posts');
+      // Use connect method to connect to the server
+      MongoClient.connect(url, function(err, client) {
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+        const collection = db.collection('videos');
+        collection.find().toArray((error, documents) => {
+        console.log(documents);
+        });
 
-      //   // end mongo connection
-      //   client.close();
-      // });
+        // end mongo connection
+        client.close();
+      });
 
 
 
