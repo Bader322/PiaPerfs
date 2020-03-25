a1d10b1ce1753e37ab272f23555a5850760617db
Abdullah Alharbi <abdullah@excelguru.ca>
2020-03-25 13:15:22 -0700

    New changes in indentation style to make readable for Abdullah

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,27 +3,40 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/pianoref';
 const router = express.Router();
 const post = require('../Models/Post')
+
 // Database Name
 const dbName = 'posts';
 
+const sView = 'posts';
 
 // route to posts controller
-router.get('/' ,(req, res, next) => {
-  res.render('posts');
-      // Use connect method to connect to the server
-      MongoClient.connect(url, function(err, client) {
-        console.log("Connected successfully to server");
-        const db = client.db(dbName);
-        const collection = db.collection('videos');
-        collection.find().toArray((error, documents) => {
-        console.log(documents);
-        });
+router.get(
+    '/',
+    (req, res, next) =>
+    {
+        res.render(sView);
 
-        // end mongo connection
-        client.close();
-      });
+        // Use connect method to connect to the server
+        MongoClient.connect(
+            url,
+            function(err, client)
+            {
+                console.log("Connected successfully to server");
+                const db = client.db(dbName);
+                const collection = db.collection('videos');
 
+                collection.find().toArray(
+                    (error, documents) =>
+                    {
+                        console.log(documents);
+                    }
+                );
 
+                // end mongo connection
+                client.close();
+            }
+        );
+    }
+);
 
-});
 module.exports = router;
